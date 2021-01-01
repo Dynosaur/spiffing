0f58a3bfd7cd6f521ff10c39b92038d3a1de188d
Dynosaur <heracassia@gmail.com>
2021-01-01 17:36:53 -0600

    Updated create-post-dialog

--- src/app/ui/components/dialogs/create-post-dialog/create-post-dialog.component.ts
+++ src/app/ui/components/dialogs/create-post-dialog/create-post-dialog.component.ts
@@ -1,33 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { PostService } from '@spiffing/services/post/post.service';
+import { PostService } from 'spiff/app/services/post.service';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-create-post-dialog',
+    selector: 'spiff-create-post-dialog',
     templateUrl: './create-post-dialog.component.html',
     styleUrls: ['./create-post-dialog.component.scss']
 })
 export class CreatePostDialogComponent {
-
+    public isPosting: boolean;
+    public errorMessage: string;
     public postTitle = new FormControl('', [Validators.required]);
     public postContent = new FormControl('', [Validators.required]);
-
-    public errorMessage: string;
-    public isPosting: boolean;
 
     constructor(private dialog: MatDialogRef<CreatePostDialogComponent>, private post: PostService) { }
 
     public async createPost(): Promise<void> {
         this.errorMessage = null;
-
         this.isPosting = true;
-        const res = await this.post.createPost(this.postTitle.value, this.postContent.value);
+        const createPostResult = await this.post.createPost(this.postTitle.value, this.postContent.value);
         this.isPosting = false;
-        if (res.success) {
-            this.dialog.close();
+        if (createPostResult) {
+            this.errorMessage = createPostResult;
         } else {
-            this.errorMessage = res.message;
+            this.dialog.close();
         }
     }
 
