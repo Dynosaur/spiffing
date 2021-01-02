990355af29eef5b1f9007960d92af6a919f9ca11
Dynosaur <heracassia@gmail.com>
2021-01-02 15:14:53 -0600

    Now makes request to get post author's username

--- src/app/ui/components/dialogs/view-post-dialog/view-post-dialog.component.ts
+++ src/app/ui/components/dialogs/view-post-dialog/view-post-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Post } from 'spiff/app/api/interface';
+import { Router } from '@angular/router';
+import { Post, User } from 'spiff/app/api/interface';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { ApiEndpointService } from 'spiff/app/api/services/api-endpoint.service';
 
@@ -9,14 +11,24 @@ import { ApiEndpointService } from 'spiff/app/api/services/api-endpoint.service'
 })
 export class ViewPostDialogComponent implements OnInit {
     post: Post;
+    author: User;
     postId: string;
 
-    constructor(private api: ApiEndpointService) { }
+    constructor(private api: ApiEndpointService, private router: Router, private dialog: MatDialogRef<ViewPostDialogComponent>) { }
 
     async ngOnInit(): Promise<void> {
         const postRequest = await this.api.getPost(this.postId);
-        if (postRequest.ok) this.post = postRequest.post;
-        else throw new Error('idk error');
+        if (postRequest.ok) {
+            this.post = postRequest.post;
+            const userRequest = await this.api.getUser(this.post.author);
+            if (userRequest.ok)
+                this.author = userRequest.user;
+        } else throw new Error('idk error');
+    }
+
+    toProfile(username: string): void {
+        this.router.navigate(['user', username]);
+        this.dialog.close();
     }
 
 }
