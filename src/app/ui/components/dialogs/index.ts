ef509129892cd36d0cea35555b6bba6733726c74
Dynosaur <heracassia@gmail.com>
2021-01-02 07:55:54 -0600

    Sorted imports

--- src/app/ui/components/dialogs/index.ts
+++ src/app/ui/components/dialogs/index.ts
@@ -1,4 +1,6 @@
+export { LoginDialogComponent } from './login-dialog';
+export { ViewPostDialogComponent } from './view-post-dialog';
+export { CreatePostDialogComponent } from './create-post-dialog';
+export { CreateAccountDialogComponent } from './create-account-dialog';
 export { ChangeUsernameDialogComponent } from './change-username-dialog';
 export { DeleteAccountConfirmDialogComponent } from './delete-account-confirm-dialog';
-export { CreateAccountDialogComponent } from './create-account-dialog';
-export { CreatePostDialogComponent } from './create-post-dialog';
