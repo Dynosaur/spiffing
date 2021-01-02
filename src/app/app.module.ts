ea26cf06ded4c28c8cf2130e66a59851ca5afe6d
Dynosaur <heracassia@gmail.com>
2021-01-02 08:19:46 -0600

    Sorted imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,49 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { StandaloneCardComponent } from './components/standalone-card/standalone-card.component';
-import { ButtonComponent, ViewCardComponent } from './ui/components';
-import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
-import { FormFieldComponent } from './components/form-field/form-field.component';
-import { ProfileComponent, SettingsComponent } from './ui/views';
-import { CreateAccountDialogComponent, DeleteAccountConfirmDialogComponent, ChangeUsernameDialogComponent,
-CreatePostDialogComponent } from './ui/components/dialogs';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
 import { DialogComponent } from './ui/components';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FormFieldComponent } from './components/form-field/form-field.component';
+import { LandingPageComponent } from 'spiff/app/ui/views/landing-page';
+import { LoginDialogComponent } from 'spiff/app/ui/components/dialogs/login-dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StandaloneCardComponent } from './components/standalone-card/standalone-card.component';
+import { ViewPostDialogComponent } from './ui/components/dialogs/view-post-dialog/view-post-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent, ViewCardComponent } from './ui/components';
+import { ProfileComponent, SettingsComponent } from './ui/views';
+import { CreateAccountDialogComponent, DeleteAccountConfirmDialogComponent, ChangeUsernameDialogComponent, CreatePostDialogComponent } from './ui/components/dialogs';
 
 @NgModule({
     declarations: [
         AppComponent,
-        LandingPageComponent,
-        NotFoundComponent,
-        StandaloneCardComponent,
         ButtonComponent,
-        CreatePostDialogComponent,
+        DialogComponent,
         ProfileComponent,
-        LoginDialogComponent,
-        CreateAccountDialogComponent,
-        FormFieldComponent,
+        NotFoundComponent,
         SettingsComponent,
         ViewCardComponent,
+        FormFieldComponent,
+        LandingPageComponent,
+        LoginDialogComponent,
+        StandaloneCardComponent,
+        ViewPostDialogComponent,
+        CreatePostDialogComponent,
+        CreateAccountDialogComponent,
         DeleteAccountConfirmDialogComponent,
-        ChangeUsernameDialogComponent,
-        DialogComponent
+        ChangeUsernameDialogComponent
     ],
     imports: [
-        BrowserAnimationsModule,
-        BrowserModule,
-        AppRoutingModule,
-        MaterialModule,
         FormsModule,
+        BrowserModule,
+        MaterialModule,
+        AppRoutingModule,
+        HttpClientModule,
         ReactiveFormsModule,
-        HttpClientModule
+        BrowserAnimationsModule
     ],
     bootstrap: [AppComponent]
 })
