2d081f0678ac95e1807937984f8bbd095584d2eb
Dynosaur <heracassia@gmail.com>
2021-01-09 17:28:23 -0600

    UI now correctly shows current status of a posts rating

--- src/app/ui/components/dialogs/view-post-dialog/view-post-dialog.component.ts
+++ src/app/ui/components/dialogs/view-post-dialog/view-post-dialog.component.ts
@@ -3,7 +3,7 @@ import { Post, User } from 'spiff/app/api/interface';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { ApiEndpointService } from 'spiff/app/api/services/api-endpoint.service';
-import { UserAccountService } from 'spiff/app/services/user-account.service';
+import { UserAccountEvent, UserAccountService } from 'spiff/app/services/user-account.service';
 
 @Component({
     selector: 'spiff-view-post-dialog',
@@ -11,9 +11,12 @@ import { UserAccountService } from 'spiff/app/services/user-account.service';
     styleUrls: ['./view-post-dialog.component.scss']
 })
 export class ViewPostDialogComponent implements OnInit {
+    loading = true;
     post: Post;
     author: User;
     postId: string;
+    liked = false;
+    disliked = false;
 
     constructor(private api: ApiEndpointService,
                 private router: Router,
@@ -25,9 +28,25 @@ export class ViewPostDialogComponent implements OnInit {
         if (postRequest.ok) {
             this.post = postRequest.post;
             const userRequest = await this.api.getUser(this.post.author);
-            if (userRequest.ok)
-                this.author = userRequest.user;
+            if (userRequest.ok) this.author = userRequest.user;
+            if (this.account.ratedMap.status === 'Loading') {
+                this.account.accountEventStream.subscribe((event: UserAccountEvent) => {
+                    if (event === 'ratedMapReady') this.getUserRating();
+                });
+            } else this.getUserRating();
+            this.loading = false;
         } else throw new Error('idk error');
+    }
+
+    getUserRating(): void {
+        if (this.account.ratedMap.map.has(this.postId)) {
+            const rating = this.account.ratedMap.map.get(this.postId);
+            this.liked = rating;
+            this.disliked = !rating;
+        } else {
+            this.liked = false;
+            this.disliked = false;
+        }
     }
 
     toProfile(username: string): void {
@@ -36,16 +55,56 @@ export class ViewPostDialogComponent implements OnInit {
     }
 
     async likePost(): Promise<void> {
-        const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, 1);
-        if (rateRequest.ok) {
-            this.post.likes++;
+        if (this.liked) {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, 0);
+            if (rateRequest.ok) {
+                this.post.likes--;
+                this.account.ratedMap.map.delete(this.postId);
+                this.liked = false;
+            }
+        } else if (this.disliked) {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, 1);
+            if (rateRequest.ok) {
+                this.post.dislikes--;
+                this.post.likes++;
+                this.liked = true;
+                this.disliked = false;
+                this.account.ratedMap.map.set(this.postId, true);
+            }
+        } else {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, 1);
+            if (rateRequest.ok) {
+                this.post.likes++;
+                this.liked = true;
+                this.account.ratedMap.map.set(this.postId, true);
+            }
         }
     }
 
     async dislikePost(): Promise<void> {
-        const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, -1);
-        if (rateRequest.ok) {
-            this.post.dislikes++;
+        if (this.disliked) {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, 0);
+            if (rateRequest.ok) {
+                this.post.dislikes--;
+                this.disliked = false;
+                this.account.ratedMap.map.delete(this.postId);
+            }
+        } else if (this.liked) {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, -1);
+            if (rateRequest.ok) {
+                this.post.dislikes++;
+                this.post.likes--;
+                this.liked = false;
+                this.disliked = true;
+                this.account.ratedMap.map.set(this.postId, false);
+            }
+        } else {
+            const rateRequest = await this.api.ratePost(this.account.username, this.account.password, this.postId, -1);
+            if (rateRequest.ok) {
+                this.post.dislikes++;
+                this.disliked = true;
+                this.account.ratedMap.map.set(this.postId, false);
+            }
         }
     }
 
