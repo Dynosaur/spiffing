5121298d5d72db97d74d0bba07c629adbe370558
Dynosaur <heracassia@gmail.com>
2021-01-09 17:29:00 -0600

    UserAccountService now keeps track of rated posts

--- src/app/services/user-account.service.ts
+++ src/app/services/user-account.service.ts
@@ -2,6 +2,8 @@ import { ApiEndpointService } from 'spiff/app/api/services/api-endpoint.service'
 import { Injectable, EventEmitter } from '@angular/core';
 import { LOCAL_STORAGE_ACCOUNT_KEY, LocalStorageService } from './local-storage.service';
 
+export type UserAccountEvent = 'ratedMapReady';
+
 @Injectable({
     providedIn: 'root'
 })
@@ -13,8 +15,13 @@ export class UserAccountService {
     isSignedIn = false;
     loginInProgress = false;
     createdTimestamp: number;
+    ratedMap: { status: 'Loading' | 'Ready'; map: Map<string, boolean>; } = {
+        status: 'Loading',
+        map: new Map()
+    };
 
     public signInEventStream = new EventEmitter<boolean>();
+    accountEventStream = new EventEmitter<UserAccountEvent>();
 
     constructor(private ls: LocalStorageService, private api: ApiEndpointService) { }
 
@@ -24,20 +31,34 @@ export class UserAccountService {
         if (authenticateRequest.ok) {
             this.username = username;
             this.password = password;
-            const getUserRequest = await this.api.getUser(this.username);
-            if (getUserRequest.ok) {
-                this.id = getUserRequest.user._id;
-                this.screenName = getUserRequest.user.screenname;
-                this.createdTimestamp = getUserRequest.user.created;
-            } else {
-                throw new Error('Could not find user after authenticating.');
-            }
-            this.ls.write(LOCAL_STORAGE_ACCOUNT_KEY, { username, password });
-            this.loginInProgress = false;
-            this.setSignInStatus(true);
+            await this.onSuccessfulLogin();
             return true;
         }
         return false;
+    }
+
+    private async onSuccessfulLogin(): Promise<void> {
+        const getUserRequest = await this.api.getUser(this.username);
+        if (getUserRequest.ok) {
+            this.id = getUserRequest.user._id;
+            this.screenName = getUserRequest.user.screenname;
+            this.createdTimestamp = getUserRequest.user.created;
+        } else throw new Error('Could not find user after authenticating.');
+        const getRatedPostsRequest = await this.api.getRatedPosts(this.username, this.password, this.id);
+        if (getRatedPostsRequest.ok) {
+            for (const rated of getRatedPostsRequest.ratedPosts.posts)
+                if (rated.rating === 1) this.ratedMap.map.set(rated._id, true);
+                else this.ratedMap.map.set(rated._id, false);
+            this.ratedMap.status = 'Ready';
+            this.accountEventStream.emit('ratedMapReady');
+        } else {
+            this.ratedMap.status = 'Ready';
+            this.accountEventStream.emit('ratedMapReady');
+            throw new Error('Could not get rated posts.');
+        }
+        this.ls.write(LOCAL_STORAGE_ACCOUNT_KEY, { username: this.username, password: this.password });
+        this.loginInProgress = false;
+        this.setSignInStatus(true);
     }
 
     public logOut(): void {
