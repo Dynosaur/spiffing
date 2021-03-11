a266f587013fb50eb9409bdd57eb1396e3d5f5e4
Dynosaur <heracassia@gmail.com>
2021-03-10 20:17:47 -0600

    Added postComment, getCmoments, rateComment

--- src/app/api/services/api.service.ts
+++ src/app/api/services/api.service.ts
@@ -1,74 +1,107 @@
 import { Injectable } from '@angular/core';
-import { ApiHttpService } from 'spiff/app/api/services/api-http.service';
+import { ApiHttpService }     from 'api/services/api-http.service';
 import { basicAuthorization } from 'spiff/app/util/basic-auth';
-import { IAuthorize, ICreatePost, IDeregister, IGetPosts, IGetRatedPosts, IGetUsers, IPatch, IRatePost, IRegister } from 'spiff/app/api/interface';
+import {
+    IAuthorize,
+    ICreatePost,
+    IDeleteUser,
+    IGetComments,
+    IGetPosts,
+    IGetRates,
+    IGetUsers,
+    IPatch,
+    IPostComment,
+    IRateComment,
+    IRatePost,
+    IRegister,
+} from 'api/interface';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-    constructor(private http: ApiHttpService) { }
+    constructor(private http: ApiHttpService) {}
 
-    async register(username: string, password: string): Promise<IRegister.Tx> {
-        return await this.http.post<IRegister.Tx>(['api', 'user', username], {}, {
+    register(username: string, password: string): Promise<IRegister.Tx> {
+        return this.http.post(['api', 'user', username], {}, {
             authorization: basicAuthorization(username, password)
         });
     }
 
-    async authorize(username: string, password: string): Promise<IAuthorize.Tx> {
-        return await this.http.post<IAuthorize.Tx>(['api', 'authorize'], {}, {
+    authorize(username: string, password: string): Promise<IAuthorize.Tx> {
+        return this.http.post(['api', 'authorize'], {}, {
             authorization: basicAuthorization(username, password)
         });
     }
 
-    async deregister(username: string, password: string): Promise<IDeregister.Tx> {
-        return await this.http.delete<IDeregister.Tx>(['api', 'user', username], {
+    deregister(username: string, password: string): Promise<IDeleteUser.Tx> {
+        return this.http.delete(['api', 'user'], {
             authorization: basicAuthorization(username, password)
         });
     }
 
-    async patch(username: string, password: string, changes: {
+    patch(username: string, password: string, changes: {
         username?: string;
         password?: string;
         screenname?: string;
     }): Promise<IPatch.Tx> {
-        return await this.http.patch<IPatch.Tx>(['api', 'user', username], changes, {
+        return this.http.patch(['api', 'user', username], changes, {
             authorization: basicAuthorization(username, password)
         });
     }
 
-    async getPosts(query: {
-        author?: string;
-        id?: string;
-        ids?: string;
-        include?: string;
-        title?: string
-    }): Promise<IGetPosts.Tx> {
-        return await this.http.get<IGetPosts.Tx>(['api', 'posts'], query);
+    getPosts(query: { author?: string; id?: string; ids?: string; include?: string; title?: string }
+    ): Promise<IGetPosts.Tx> {
+        return this.http.get(['api', 'posts'], query);
     }
 
-    async createPost(username: string, password: string, title: string, content: string): Promise<ICreatePost.Tx> {
-        return await this.http.post<ICreatePost.Tx>(['api', 'post'], { title, content },
+    createPost(username: string, password: string, title: string, content: string): Promise<ICreatePost.Tx> {
+        return this.http.post(['api', 'post'], { title, content },
             { authorization: basicAuthorization(username, password) }
         );
     }
 
-    async ratePost(username: string, password: string, postId: string, rating: -1 | 0 | 1): Promise<IRatePost.Tx> {
-        return await this.http.post<IRatePost.Tx>(['api', 'rate', 'post', postId], { rating },
+    ratePost(username: string, password: string, postId: string, rating: -1 | 0 | 1): Promise<IRatePost.Tx> {
+        return this.http.post(['api', 'rate', 'post', postId], { rating },
             { authorization: basicAuthorization(username, password) }
         );
     }
 
-    async getRatedPosts(username: string, password: string, uid: string): Promise<IGetRatedPosts.Tx> {
-        return await this.http.get<IGetRatedPosts.Tx>(['api', 'rated', uid], {},
+    getRates(username: string, password: string, uid: string): Promise<IGetRates.Tx> {
+        return this.http.get(['api', 'rated', uid], {},
             { authorization: basicAuthorization(username, password) }
         );
     }
 
-    async getUsers(query: { id?: string; ids?: string[]; username?: string; usernames?: string[]; } = {}): Promise<IGetUsers.Tx> {
-        return await this.http.get<IGetUsers.Tx>(['api', 'users'], {
+    getUsers(query: { id?: string; ids?: string[]; username?: string; usernames?: string[]; } = {}
+    ): Promise<IGetUsers.Tx> {
+        return this.http.get(['api', 'users'], {
             ... query.id  && { id: query.id },
             ... query.ids && { ids: query.ids.join(',') },
             ... query.username  && { username: query.username },
             ... query.usernames && { usernames: query.usernames.join(',') }
         });
+    }
+
+    postComment(username: string, password: string, parentType: 'post' | 'comment', parentId: string, content: string
+    ): Promise<IPostComment.Tx> {
+        return this.http.post(
+            ['api', 'comment', parentType, parentId],
+            { content },
+            { authorization: basicAuthorization(username, password) }
+        );
+    }
+
+    getComments(query: { parent?: { type: 'comment' | 'post'; id: string; }; author?: string; } = {},
+    includeAuthorUser?: boolean): Promise<IGetComments.Tx> {
+        return this.http.get(['api', 'comments'], {
+            ...query.parent && { parentType: query.parent.type, parentId: query.parent.id },
+            ...query.author && { author: query.author },
+            ...includeAuthorUser !== undefined && { include: 'authorUser' }
+        });
+    }
+
+    rateComment(username: string, password: string, commentId: string, rating: -1 | 0 | 1): Promise<IRateComment.Tx> {
+        return this.http.post(['api', 'rate', 'comment', commentId], { rating },
+            { authorization: basicAuthorization(username, password) }
+        );
     }
 }
